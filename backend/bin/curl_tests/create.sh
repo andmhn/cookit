003e2da8d777cdddc5ec592e527788ab236d1598
Anand Mohan <andmhn@outlook.com>
2024-07-09 09:10:29 +0530

    reverting commited binaries

--- backend/bin/curl_tests/create.sh
+++
@@ -1,2 +0,0 @@
-curl -X POST 127.0.0.0:8080/register  -H 'Content-Type: application/json' -d '{"fullname":"Example", "email":"example@email.com", "password":"1234","joindate":"28-6-2024"}'
-curl -X POST 127.0.0.0:8080/register  -H 'Content-Type: application/json' -d '{"fullname":"Anand Mohan", "password":"ry74reh","joindate":"28-06-2024", "email":"anandmohan@email.com"}'
--- backend/bin/curl_tests/read.sh
+++
@@ -1,1 +0,0 @@
-curl --silent -u example@email.com:1234 -X GET http://localhost:8080/findAll | jq
